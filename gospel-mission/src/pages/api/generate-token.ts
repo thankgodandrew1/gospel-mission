a5989d580f458817a7928de78e1951abb873508a
thankgodandrew1 <and22046@byui.edu>
2025-01-15 14:01:03 +0100

    production failure: second fix

--- gospel-mission/src/pages/api/generate-token.ts
+++ gospel-mission/src/pages/api/generate-token.ts
@@ -1,9 +1,14 @@
-// src/pages/api/generate-token.ts
 import { NextApiRequest, NextApiResponse } from 'next';
 import jwt from 'jsonwebtoken';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const secret = process.env.JWT_SECRET;
+
+  if (!secret) {
+    // Handle the case when the secret is not defined
+    return res.status(500).json({ error: 'JWT_SECRET is not defined' });
+  }
+
   const token = jwt.sign({ data: 'example' }, secret, { expiresIn: '1h' });
   res.status(200).json({ token });
 }
